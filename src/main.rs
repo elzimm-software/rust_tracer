c5f0489830a7f2c3fc768a960c4ffe9f445f151f
arcaneJesus <elijahz0409@gmail.com>
2024-09-04 15:36:03 -0400

    Added progress indicators.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::io;
 use std::io::Write;
 
 fn main() -> std::io::Result<()> {
@@ -10,6 +11,8 @@ fn main() -> std::io::Result<()> {
     output.write_fmt(format_args!("P3\n{} {}\n255\n", image_width, image_height))?;
 
     for j in 0..image_height {
+        print!("\rScanlines remaining: {} ", image_height - j);
+        io::stdout().flush()?;
         for i in 0..image_width {
             let r = i as f64 / (image_width-1) as f64;
             let g = j as f64 / (image_height-1) as f64;
@@ -22,6 +25,7 @@ fn main() -> std::io::Result<()> {
             output.write_fmt(format_args!("{} {} {}\n", ir,ig,ib))?;
         }
     }
+    print!("\rDone.                 \n");
 
     Ok(())
 }
