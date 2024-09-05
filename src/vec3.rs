2464ffa1ef949a0a59c5afe6959cb8010b6c46a1
Elijah Zimmerman <elijahz0409@gmail.com>
2024-09-05 02:30:11 -0400

    Added operator overrides for Vec3

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,4 +1,8 @@
-use std::ops::{AddAssign, Index, Neg};
+use std::fmt::{Debug, Display, Formatter};
+use std::num::{Add, Sub};
+use std::ops::{Add, AddAssign, DivAssign, Index, Mul, MulAssign, Neg, Sub};
+
+type Point3 = Vec3;
 
 pub struct Vec3 {
     pub x: f64,
@@ -20,6 +24,76 @@ impl Vec3 {
             x: 0.0,
             y: 0.0,
             z: 0.0,
+        }
+    }
+
+    pub fn len(&self) -> f64 {
+        self.len_sqrd().sqrt()
+    }
+
+    pub fn len_sqrd(&self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+}
+
+impl Display for Vec3 {
+    #[inline]
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} {}", self.x, self.y, self.z)
+    }
+}
+
+impl Debug for Vec3 {
+    #[inline]
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "x: {}, y: {}, z: {}", self.x, self.y, self.z)
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+            z: self.z * rhs.z,
+        }
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Self {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
         }
     }
 }
@@ -61,3 +135,24 @@ impl AddAssign for Vec3 {
     }
 }
 
+impl MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        self.x *= rhs;
+        self.y *= rhs;
+        self.z *= rhs;
+    }
+}
+
+impl MulAssign<f64> for &mut Vec3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        self.x *= rhs;
+        self.x *= rhs;
+        self.z *= rhs;
+    }
+}
+
+impl DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, rhs: f64) {
+       self *= 1.0/rhs;
+    }
+}
